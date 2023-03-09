6544dfc8c760456d33efcce880dc3bcb29510141
GianfrancoGhiani <gianfranco.ghiani.gg@gmail.com>
2023-03-09 14:32:04 +0100

    Snack 3

--- src/Snack3.java
+++ src/Snack3.java
@@ -1,5 +1,10 @@
 public class Snack3 {
     public static void main(String[] args) {
-
+    int[] arrayInt = {1,2,3,3,2,3,3,3,2,3,2};
+    int sum =0 ;
+    for (int i = 1; i < arrayInt.length; i+=2) {
+        sum+=arrayInt[i];
+    }
+    System.out.println(sum);
     }
 }
