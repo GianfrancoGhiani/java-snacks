9de6ac2865c40c663e0be70e7dd5a6803303c83d
GianfrancoGhiani <gianfranco.ghiani.gg@gmail.com>
2023-03-09 14:13:47 +0100

    Snack 1

--- src/Snack1.java
+++ src/Snack1.java
@@ -1,2 +1,14 @@
+import java.util.Scanner;
+
 public class Snack1 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("insert a number");
+        int userNum = Integer.parseInt(scan.nextLine());
+        if ((userNum % 2) == 0){
+            System.out.println(userNum);
+        }else {
+            System.out.println(userNum+1);
+        }
+    }
 }
